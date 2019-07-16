c8e0bdacee7ba78fd6d738cc07d0a1b4b2be4c1f
zc <1710121246@vip.henu.edu.cn>
2019-07-16 10:57:29 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Text widget',
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            child: new Text('hello', style: TextStyle(fontSize: 40.0)),
+            alignment: Alignment.center,
+            width: 500.0,
+            height: 400,
+            color: Colors.lightBlue ,
+          ),
+        ),
+      ),
+    );
+  }
+}
