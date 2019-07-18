ad6711c40ec8cc9061fe6e01a733ae2d21bb9e39
lishilong <1825010019@vip.henu.edu.cn>
2019-07-18 17:58:09 +0800

    add enter

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget{
         body: Center(
           child: Container(
             child: new Text('hello', style: TextStyle(fontSize: 40.0)),
+            
             alignment: Alignment.center,
             width: 500.0,
             height: 400,
