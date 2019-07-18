b436108526deeb515b6f3efa214b4007afc9d71d
xushiqiang <1825010208@vip.henu.edu.cn>
2019-07-18 19:23:58 +0800

    更新了readme文件

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,13 @@ void main () => runApp(MyApp());
 class MyApp extends StatelessWidget{
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context){ 
     return MaterialApp(
       title: 'Text widget',
       home: Scaffold(
         body: Center(
           child: Container(
             child: new Text('hello', style: TextStyle(fontSize: 40.0)),
-            
-
-
             alignment: Alignment.center,
             width: 500.0,
             height: 400,
