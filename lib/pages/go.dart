0c13467fc57ad9a5607aebe0ef2c029fdf32e457
yzp <1825010096@vip.henu.edu.cn>
2019-07-23 10:10:27 +0800

    对所有页面都进行了整合，解决了historydetail返回bug

---
+++ lib/pages/go.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'speedtest.dart';
+
+class GoPage extends StatefulWidget {
+  GoPage({Key key}) : super(key: key);
+
+  _GoPageState createState() => _GoPageState();
+}
+
+class _GoPageState extends State<GoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Color.fromRGBO(21, 20, 36, 0.99),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            //上面的go,圆形盘
+            Container(
+              child: Container(
+                height: 230.0,
+                width: 230.0,
+                child: RaisedButton(
+                  child: Text(
+                    'GO',
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                    style: TextStyle(
+                      fontSize: 50.0,
+                      color: const Color(0xffFFFFFF),
+                      fontWeight: FontWeight.bold,
+                      fontFamily: '微软雅黑',
+                      letterSpacing: 3.0,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(66.5),
+                  splashColor: Color.fromRGBO(78, 201, 176, 0.7), //水波纹颜色
+                  shape: CircleBorder(
+                      side: BorderSide(color: Color.fromRGBO(21, 20, 36, 1))),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => SpeedtestPage()));
+                  },
+
+                  color: Color.fromRGBO(21, 20, 36, 1),
+                ),
+                alignment: Alignment.center,
+                decoration: new BoxDecoration(
+                    border:
+                        new Border.all(width: 4.0, color: Color(0xff068586)),
+                    color: Color.fromRGBO(21, 20, 36, 1),
+                    borderRadius: BorderRadius.circular(150)),
+              ),
+            ),
+
+            SizedBox(height: 20.0),
+
+            //中间的数据，位置啊，热点类型啊四个
+            Container(
+              padding: EdgeInsets.fromLTRB(30.0, 60.0, 0.0, 0.0),
+              child: Column(
+                //2个child 竖直 排列
+                children: <Widget>[
+                  // 2 个child 水平 排列
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(right: 10.0),
+                        child: Icon(
+                          Icons.filter_tilt_shift,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                      ),
+                      Text(
+                        "Type:  wifi",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height:15),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        child: Icon(
+                          Icons.alternate_email,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                        padding: EdgeInsets.only(right: 10.0),
+                      ),
+                      Text(
+                        "Server Name:  chinese",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height:15),
+                  Row(
+                    children: <Widget>[
+                      
+                      Container(
+                        child: Icon(
+                          Icons.wifi_tethering,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                        padding: EdgeInsets.only(right: 10.0),
+                      ),
+                      Text(
+                        "IP:  127.192.4.12",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height:15),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                        padding: EdgeInsets.only(right: 10.0),
+                      ),
+                      Text(
+                        "Location:  kaifeng",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            
+            //文字到底部导航的距离可以把文字挤上去
+            SizedBox(height: 40,)
+
+          ],
+        ),
+      );
+  }
+}
--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'speed.dart';
-import 'history.dart';
+
 class SettingPage extends StatefulWidget {
   SettingPage({Key key}) : super(key: key);
 
@@ -8,381 +7,205 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
-  @override 
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Color.fromRGBO(21, 20, 36, 1),
-             centerTitle: true,
-            title:  Text(
-                'S E T T I N G',
-                style: TextStyle(
+    return Container(
+      child: Column(
+        children: <Widget>[
+          //testing//////////////
+          Container(
+            alignment: Alignment(-1, 0),
+            padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
+            child: Text(
+              'TESTING',
+              style: TextStyle(
+                  color: Colors.white,
                   fontWeight: FontWeight.w900,
-                  color: Color.fromRGBO(78, 201, 176, 1)
-                  ),
+                  fontSize: 21.0),
+            ),
+          ),
+          //分割线///////////////
+          Container(
+            decoration: new BoxDecoration(
+                border: new Border(
+                    bottom: Divider.createBorderSide(context,
+                        width: 1.0, color: Colors.grey))),
+          ),
+          //第一行///////////////
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(
+                height: 50,
               ),
-            ),
-        body: Container(
-          child: Column(children: <Widget>[
-            //testing//////////////
-        Container(
-          alignment: Alignment(-1, 0),
-          padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
-          child: Text(
-            'TESTING',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w900,
-            fontSize: 21.0
+              Container(
+                  width: 60.0,
+                  height: 50.0,
+                  alignment: Alignment(0, 0),
+                  decoration: new BoxDecoration(
+                      border: new Border(
+                          bottom: Divider.createBorderSide(
+                    context,
+                    width: 3.0,
+                  ))),
+                  child: Text(
+                    "Units",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w100,
+                    ),
+                  )),
+              Container(
+                  width: 60.0,
+                  height: 50.0,
+                  alignment: Alignment(0, 0),
+                  decoration: new BoxDecoration(
+                      border: new Border(
+                          bottom: Divider.createBorderSide(context,
+                              width: 3.0, color: Color(0xff1FF8E8)))),
+                  child: Text(
+                    "Mbps",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  )),
+              Container(
+                  width: 60.0,
+                  height: 50.0,
+                  alignment: Alignment(0, 0),
+                  decoration: new BoxDecoration(
+                      border: new Border(
+                          bottom: Divider.createBorderSide(
+                    context,
+                    width: 3.0,
+                  ))),
+                  child: Text(
+                    "MB/s",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  )),
+              Container(
+                  width: 60.0,
+                  height: 50.0,
+                  alignment: Alignment(0, 0),
+                  decoration: new BoxDecoration(
+                      border: new Border(
+                          bottom: Divider.createBorderSide(
+                    context,
+                    width: 3.0,
+                  ))),
+                  child: Text(
+                    "kB/s",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  )),
+            ],
           ),
+          //分割线///////////////
+          Container(
+            decoration: new BoxDecoration(
+                border: new Border(
+                    bottom: Divider.createBorderSide(context,
+                        width: 1.0, color: Colors.grey))),
           ),
-        ),
-        //分割线///////////////
-        Container(
-           decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width: 1.0,
-               color: Colors.grey
-               ) 
-             )
-           ),
-        ),
-        //第一行///////////////
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //第二行/////////////////
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-                 
-            SizedBox(
-            height: 50,
-             ),
-            Container(
-              width: 60.0,
-              height: 50.0,
-              alignment: Alignment(0,0),
-              decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width:3.0,
-               ) 
-             )
-           ),
-             child:Text(
-                  "Units",
-                  style: TextStyle(
-                    color: Colors.white,
-                    
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w100,
-                
-                ),
-              )
-            ),
-                
-                 Container(
-              width: 60.0,
-               height: 50.0,
-              alignment: Alignment(0,0),
-              decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width:3.0,
-               color:Color(0xff1FF8E8)
-               ) 
-             )
-           ),
-             child:Text(
-                  "Mbps",
-                  style: TextStyle(
-                    color: Colors.white,
-                    
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.w900,
-                
-                ),
-              )
-            ), Container(
-              width: 60.0,
-               height: 50.0,
-              alignment: Alignment(0,0),
-              decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width:3.0,
-               ) 
-             )
-           ),
-             child:Text(
-                  "MB/s",
-                  style: TextStyle(
-                    color: Colors.white,
-                    
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.w900,
-                
-                ),
-              )
-            ),
-                 Container(
-              width: 60.0,
-               height: 50.0,
-              alignment: Alignment(0,0),
-              decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width:3.0,
-               ) 
-             )
-           ),
-             child:Text(
-                  "kB/s",
-                  style: TextStyle(
-                    color: Colors.white,
-                    
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.w900,
-                
-                ),
-              )
-            ),
-              ],
-            ),
-        //分割线///////////////
-        Container(
-           decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width: 1.0,
-               color: Colors.grey
-               ) 
-             )
-           ),
-        ),
-        //第二行/////////////////
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-                 
-            SizedBox(
-            height: 50,
-             ),
-            Container(
-              width: 60.0,
-              height: 50.0,
-              alignment: Alignment(0,0),
-              decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width:3.0,
-               ) 
-             )
-           ),
-             child:Text(
-                  "Scale",
-                  style: TextStyle(
-                    color: Colors.white,
-                    
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w100,
-                
-                ),
-              )
-            ),
-                
-                 Container(
-              width: 60.0,
-              height: 50.0,
-              alignment: Alignment(0,0),
-              decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width:3.0,
-               
-               color:Color(0xff1FF8E8)
-               ) 
-             )
-           ),
-             child:Text(
-                  "100",
-                  style: TextStyle(
-                    color: Colors.white,
-                    
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w900,
-                
-                ),
-              )
-            ), Container(
-              width: 60.0,
-              height: 50.0,
-              alignment: Alignment(0,0),
-              decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width:3.0,
-               ) 
-             )
-           ),
-             child:Text(
-                  "500",
-                  style: TextStyle(
-                    color: Colors.white,
-                    
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w900,
-                
-                ),
-              )
-            ),
-                 Container(
-              width: 60.0,
-              height: 50.0,
-              alignment: Alignment(0,0),
-              decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width:3.0,
-               ) 
-             )
-           ),
-             child:Text(
-                  "1000",
-                  style: TextStyle(
-                    color: Colors.white,
-                    
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w900,
-                
-                ),
-              )
-            ),
-              ],
-            ),
-        //分割线///////////////
-        Container(
-           decoration: new BoxDecoration(
-            
-           border: new Border(
-             bottom:Divider.createBorderSide(
-               context,
-               width: 1.0,
-               color: Colors.grey
-               ) 
-             )
-           ),
-        ),
-        ///////////////////// 
-            SizedBox(height:454),
-            
-
-
-
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  color: Color.fromRGBO(21, 20, 36, 1),
-                  height: 80,
-                  width: 120,
-                  child: RaisedButton.icon(
-                    label: Text(
-                      '',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                  width: 60.0,
+                  height: 50.0,
+                  alignment: Alignment(0, 0),
+                  decoration: new BoxDecoration(
+                      border: new Border(
+                          bottom: Divider.createBorderSide(
+                    context,
+                    width: 3.0,
+                  ))),
+                  child: Text(
+                    "Scale",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w100,
                     ),
-                    splashColor: Color.fromRGBO(78, 201, 176, 0.7), //水波纹颜色
-                    icon: Icon(
-                      Icons.network_check,
-                      size: 40,
-                      color: Colors.grey,
+                  )),
+              Container(
+                  width: 60.0,
+                  height: 50.0,
+                  alignment: Alignment(0, 0),
+                  decoration: new BoxDecoration(
+                      border: new Border(
+                          bottom: Divider.createBorderSide(context,
+                              width: 3.0, color: Color(0xff1FF8E8)))),
+                  child: Text(
+                    "100",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w900,
                     ),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0)), //加了一个小圆角
-                    onPressed: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => SpeedPage()));
-                    },
-                    color: Color.fromRGBO(21, 20, 36, 1),
-                  ),
-                ),
-                Container(
-                  color: Color.fromRGBO(21, 20, 36, 1),
-                  height: 80,
-                  width: 120,
-                  child: RaisedButton.icon(
-                    label: Text(
-                      '',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
+                  )),
+              Container(
+                  width: 60.0,
+                  height: 50.0,
+                  alignment: Alignment(0, 0),
+                  decoration: new BoxDecoration(
+                      border: new Border(
+                          bottom: Divider.createBorderSide(
+                    context,
+                    width: 3.0,
+                  ))),
+                  child: Text(
+                    "500",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w900,
                     ),
-                    splashColor: Color.fromRGBO(78, 201, 176, 0.7), //水波纹颜色
-                    icon: Icon(
-                      Icons.history,
-                      size: 40,
-                      color: Colors.grey,
+                  )),
+              Container(
+                  width: 60.0,
+                  height: 50.0,
+                  alignment: Alignment(0, 0),
+                  decoration: new BoxDecoration(
+                      border: new Border(
+                          bottom: Divider.createBorderSide(
+                    context,
+                    width: 3.0,
+                  ))),
+                  child: Text(
+                    "1000",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w900,
                     ),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0)), //加了一个小圆角
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => HistoryPage()));
-                    },
-                    color: Color.fromRGBO(21, 20, 36, 1),
-                  ),
-                ),
-                Container(
-                  color: Color.fromRGBO(21, 20, 36, 1),
-                  height: 80,
-                  width: 120,
-                  child: RaisedButton.icon(
-                    label: Text(
-                      '',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                    splashColor: Color.fromRGBO(78, 201, 176, 0.7), //水波纹颜色
-                    icon: Icon(
-                      Icons.settings,
-                      size: 40,
-                      color: Colors.grey,
-                    ),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0)), //加了一个小圆角
-                    onPressed: () {
-                      Null;
-                    },
-                    color: Color.fromRGBO(78, 201, 176, 0.5),
-                  ),
-                ),////
-              ],
-            ), 
-          ],),
-        ),
-        backgroundColor: Color.fromRGBO(21, 20, 36, 0.99),
-        );
+                  )),
+            ],
+          ),
+          //分割线///////////////
+          Container(
+            decoration: new BoxDecoration(
+                border: new Border(
+                    bottom: Divider.createBorderSide(context,
+                        width: 1.0, color: Colors.grey))),
+          ),
+          /////////////////////
+        ],
+      ),
+    );
   }
 }
